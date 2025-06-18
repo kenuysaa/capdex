3d6f6af3fec1d67d929cb439294f5dc621ae6234
kenuysaa <guilhermesaavedranunes@gmail.com>
2025-06-18 15:23:57 -0400

    feat: Adiciona modelos de dados para Embarcacao, Encomenda, Localizacao e Usuario
    Este commit introduz as classes de dados que representam as entidades principais da aplicação:
    
    - **Embarcacao:** Contém informações sobre as embarcações, incluindo identificadores, nome, CNPJ, imagem, detalhes do setor, pontos de partida e chegada, e o ID do proprietário.
    - **Encomenda:** Representa as encomendas, com detalhes como ID, descrição, imagem, status, ID da embarcação associada e CPFs do remetente e destinatário.
    - **Localizacao:** Define a estrutura para armazenar dados de localização, incluindo precisão, latitude, longitude, velocidade e timestamp.
    - **Usuario:** Modela os usuários da aplicação, com informações como ID, se é proprietário, nome, e-mail e CPF.

---
+++ app/src/main/java/com/example/capdex/data/model/Embarcacao.kt
@@ -0,0 +1,13 @@
+package com.example.capdex.data.model
+
+data class Embarcacao(
+    val idEmbarcacao: String = "",
+    val nomeEmbarcacao: String = "",
+    val cnpj: String = "",
+    val img: String = "",
+    val nomeSetor: String = "",
+    val senhaSetor: String = "",
+    val pontoPartida: String = "",
+    val pontoChegada: String = "",
+    val proprietarioId: String = ""
+)
